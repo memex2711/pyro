d60f7ee6ecc85cb36c36edb8b5c5ef2291c63caa
kynandevs <kynandevs@gmail.com>
2026-01-01 19:18:40 +0700

    gini

--- pyrogram/types/inline_mode/inline_query_result_cached_sticker.py
+++ pyrogram/types/inline_mode/inline_query_result_cached_sticker.py
@@ -17,7 +17,9 @@
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
 import pyrogram
-from pyrogram import raw, types
+
+from typing import Optional
+from pyrogram import raw, types, utils, enums
 from .inline_query_result import InlineQueryResult
 from ...file_id import FileId
 
@@ -47,17 +49,26 @@ class InlineQueryResultCachedSticker(InlineQueryResult):
         self,
         sticker_file_id: str,
         id: str = None,
+        caption: str = "",
+        parse_mode: Optional["enums.ParseMode"] = None,
+        caption_entities: list["types.MessageEntity"] = None,
         reply_markup: "types.InlineKeyboardMarkup" = None,
         input_message_content: "types.InputMessageContent" = None
     ):
         super().__init__("sticker", id, input_message_content, reply_markup)
 
         self.sticker_file_id = sticker_file_id
+        self.caption = caption
+        self.parse_mode = parse_mode
+        self.caption_entities = caption_entities
         self.reply_markup = reply_markup
         self.input_message_content = input_message_content
 
     async def write(self, client: "pyrogram.Client"):
         file_id = FileId.decode(self.sticker_file_id)
+        message, entities = (await utils.parse_text_entities(
+            client, self.caption, self.parse_mode, self.caption_entities
+        )).values()
 
         return raw.types.InputBotInlineResultDocument(
             id=self.id,
@@ -72,7 +83,8 @@ class InlineQueryResultCachedSticker(InlineQueryResult):
                 if self.input_message_content
                 else raw.types.InputBotInlineMessageMediaAuto(
                     reply_markup=await self.reply_markup.write(client) if self.reply_markup else None,
-                    message="",
+                    message=message,
+                    entities=entities
                 )
             )
         )
