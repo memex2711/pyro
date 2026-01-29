fb8634bc6b35a6eb616a1aebef829fd991e90e63
kynandevs <kynandevs@gmail.com>
2026-01-30 00:55:16 +0700

    .

--- pyrogram/enums/chat_action.py
+++ pyrogram/enums/chat_action.py
@@ -23,50 +23,50 @@ from .auto_name import AutoName
 class ChatAction(AutoName):
     """Chat action enumeration used in :obj:`~pyrogram.types.ChatEvent`."""
 
-    TYPING = raw.functions.SendMessageTypingAction
+    TYPING = raw.types.SendMessageTypingAction
     "Typing text message"
 
-    UPLOAD_PHOTO = raw.functions.SendMessageUploadPhotoAction
+    UPLOAD_PHOTO = raw.types.SendMessageUploadPhotoAction
     "Uploading photo"
 
-    RECORD_VIDEO = raw.functions.SendMessageRecordVideoAction
+    RECORD_VIDEO = raw.types.SendMessageRecordVideoAction
     "Recording video"
 
-    UPLOAD_VIDEO = raw.functions.SendMessageUploadVideoAction
+    UPLOAD_VIDEO = raw.types.SendMessageUploadVideoAction
     "Uploading video"
 
-    RECORD_AUDIO = raw.functions.SendMessageRecordAudioAction
+    RECORD_AUDIO = raw.types.SendMessageRecordAudioAction
     "Recording audio"
 
-    UPLOAD_AUDIO = raw.functions.SendMessageUploadAudioAction
+    UPLOAD_AUDIO = raw.types.SendMessageUploadAudioAction
     "Uploading audio"
 
-    UPLOAD_DOCUMENT = raw.functions.SendMessageUploadDocumentAction
+    UPLOAD_DOCUMENT = raw.types.SendMessageUploadDocumentAction
     "Uploading document"
 
-    FIND_LOCATION = raw.functions.SendMessageGeoLocationAction
+    FIND_LOCATION = raw.types.SendMessageGeoLocationAction
     "Finding location"
 
-    RECORD_VIDEO_NOTE = raw.functions.SendMessageRecordRoundAction
+    RECORD_VIDEO_NOTE = raw.types.SendMessageRecordRoundAction
     "Recording video note"
 
-    UPLOAD_VIDEO_NOTE = raw.functions.SendMessageUploadRoundAction
+    UPLOAD_VIDEO_NOTE = raw.types.SendMessageUploadRoundAction
     "Uploading video note"
 
-    PLAYING = raw.functions.SendMessageGamePlayAction
+    PLAYING = raw.types.SendMessageGamePlayAction
     "Playing game"
 
-    CHOOSE_CONTACT = raw.functions.SendMessageChooseContactAction
+    CHOOSE_CONTACT = raw.types.SendMessageChooseContactAction
     "Choosing contact"
 
-    SPEAKING = raw.functions.SpeakingInGroupCallAction
+    SPEAKING = raw.types.SpeakingInGroupCallAction
     "Speaking in group call"
 
-    IMPORT_HISTORY = raw.functions.SendMessageHistoryImportAction
+    IMPORT_HISTORY = raw.types.SendMessageHistoryImportAction
     "Importing history"
 
-    CHOOSE_STICKER = raw.functions.SendMessageChooseStickerAction
+    CHOOSE_STICKER = raw.types.SendMessageChooseStickerAction
     "Choosing sticker"
 
-    CANCEL = raw.functions.SendMessageCancelAction
+    CANCEL = raw.types.SendMessageCancelAction
     "Cancel ongoing chat action"
