3da8219a1b3b4ea414245fbdf540cb8d86ab2814
kynandevs <kynandevs@gmail.com>
2026-05-15 20:49:43 +0700

    enums ButtonStyle

--- pyrogram/enums/__init__.py
+++ pyrogram/enums/__init__.py
@@ -16,6 +16,7 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
+from .button_style import ButtonStyle
 from .chat_action import ChatAction
 from .chat_event_action import ChatEventAction
 from .chat_member_status import ChatMemberStatus
@@ -39,6 +40,7 @@ from .story_privacy import StoryPrivacy
 from .user_status import UserStatus
 
 __all__ = [
+    'ButtonStyle',
     'ChatAction',
     'ChatEventAction',
     'ChatMemberStatus',
